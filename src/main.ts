43c05af5bd0f014a1730051be53fe7091ab3f010
Alexei K <alexdevelops@outlook.com>
2024-03-22 09:12:29 +0300

    Optimize screen-md

--- src/main.ts
+++ src/main.ts
@@ -5,13 +5,20 @@ import Swiper from "swiper/bundle";
 import "swiper/css";
 import "./style.scss";
 
+const BREAKPOINT_MD = 1024;
+
 const reviewsSlider = new Swiper(".swiper-container", {
     spaceBetween: 24,
-    slidesPerView: 2,
+    slidesPerView: 1,
     navigation: {
         nextEl: ".swiper-button-next",
         prevEl: ".swiper-button-prev",
     },
+    breakpoints: {
+        [BREAKPOINT_MD]: {
+            slidesPerView: 2
+        }
+    }
 });
 
 Fancybox.bind();
